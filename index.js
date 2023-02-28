6f21f7f90b8894b17fa1524235fba03a4aa0977f
ajayahara <abak00350@gmail.com>
2023-02-28 14:59:52 +0530

    mock12

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app=express();
 app.use(express.json());
 app.use(cors())
 app.get("/",()=>{
-    resizeBy.send({"msg":"home page"})
+    res.send({"msg":"home page"})
 })
 app.use("/ad",AdRoute);
 app.listen(process.env.Port,async ()=>{
